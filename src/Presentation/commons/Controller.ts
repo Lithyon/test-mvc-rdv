da9cf6ea3a52358b0787c9a5c0429eb1ab0e33ec
Alexandre Pacaud <apacaud.externe@macif.fr>
2022-04-12 13:51:10 +0200

    remove useless MVC

--- src/Presentation/commons/Controller.ts
+++
@@ -1,8 +0,0 @@
-export type StateChangedHandler = () => void;
-
-export default interface Controller<T> {
-    onLoad():void;
-    get state():T;
-    subscribeStateChanged(onStateChanged:StateChangedHandler):void;
-    unsubscribeStateChanged(onStateChanged:StateChangedHandler):void;
-}
--- src/Presentation/hooks/useInitContexte.ts
+++
@@ -1,6 +0,0 @@
-import Controller from "../commons/Controller";
-import {useEffect} from "react";
-
-export default function useInitContexte<T>(controller:Controller<T>) {
-    useEffect(() => controller.onLoad(), [controller]);
-}
