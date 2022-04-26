6cb4e622c6f3fd904e4d1f2aa800935aa02b783b
Alexandre Pacaud <apacaud.externe@macif.fr>
2022-04-26 11:25:20 +0200

    feat: gestion domaine sante

--- src/Domain/Repository/Data/Enum/Demande.ts
+++ src/Domain/Repository/Data/Enum/Demande.ts
@@ -1,15 +1,19 @@
 export const TypeDemande = {
-  AUTRE: "99",
-  DEVIS: "01",
-  SOUSCRIPTION: "02",
-  MODIFICATION_CONTRAT: "03",
-  SINISTRE: "04",
+    AUTRE: "99",
+    DEVIS: "01",
+    SOUSCRIPTION: "02",
+    MODIFICATION_CONTRAT: "03",
+    SINISTRE: "04",
 }
 
-export const DEMANDES_HORS_SINISTRE = [
-  TypeDemande.AUTRE,
-  TypeDemande.DEVIS,
-  TypeDemande.SOUSCRIPTION,
-  TypeDemande.MODIFICATION_CONTRAT,
-  TypeDemande.SINISTRE,
+export const DEMANDES_HORS_SNISTRE = [
+    TypeDemande.AUTRE,
+    TypeDemande.DEVIS,
+    TypeDemande.SOUSCRIPTION,
+    TypeDemande.MODIFICATION_CONTRAT,
+];
+
+export const DEMANDES_DEFAULT = [
+    ...DEMANDES_HORS_SNISTRE,
+    TypeDemande.SINISTRE,
 ];
--- src/Domain/Repository/DemandeRepositoryImpl.ts
+++ src/Domain/Repository/DemandeRepositoryImpl.ts
@@ -1,25 +1,30 @@
-import Demande, { DemandeEtat } from "../Model/Demande";
-import { DemandeRepository } from "./DemandeRepository";
+import Demande from "../Model/Demande";
+import {DemandeRepository} from "./DemandeRepository";
 import DemandeDataSource from "./Data/DemandeDataSource";
-import { DEMANDES_HORS_SINISTRE } from "./Data/Enum/Demande";
-import { TypeDomaine } from "./Data/Enum/Domaine";
+import {DEMANDES_DEFAULT, DEMANDES_HORS_SNISTRE} from "./Data/Enum/Demande";
+import {TypeDomaine} from "./Data/Enum/Domaine";
+import {DemandeEntity} from "./Data/API/Entity/DemandeAPIEntity";
 
 export class DemandeRepositoryImpl implements DemandeRepository {
-  private _dataSource: DemandeDataSource;
+    private _dataSource: DemandeDataSource;
 
-  constructor(datasource: DemandeDataSource) {
-    this._dataSource = datasource;
-  }
-
-  async getDemandes(domaineSelected: string) {
-    if (domaineSelected === TypeDomaine.PRO) {
-      return new Demande([]);
+    constructor(datasource: DemandeDataSource) {
+        this._dataSource = datasource;
     }
-    const codificationDS = await this._dataSource.getDemandes();
-    return new Demande(this._codificationsFilter(codificationDS.codes));
-  }
 
-  private _codificationsFilter(codes: Array<DemandeEtat>) {
-    return codes.filter((item) => DEMANDES_HORS_SINISTRE.includes(item.code));
-  }
+    async getDemandes(domaineSelected: string) {
+        switch (domaineSelected) {
+            case TypeDomaine.PRO:
+                return new Demande([]);
+            case TypeDomaine.SANTE:
+                return new Demande(this._codificationsFilter(await this._dataSource.getDemandes(), DEMANDES_HORS_SNISTRE));
+            default:
+                return new Demande(this._codificationsFilter(await this._dataSource.getDemandes(), DEMANDES_DEFAULT));
+        }
+    }
+
+    private _codificationsFilter(demandes: DemandeEntity, filters: Array<string>) {
+        const {codes} = demandes;
+        return codes.filter((item) => filters.includes(item.code));
+    }
 }
