95d5a4fa643e18ec9cb500dd474f242fd641c174
Alexandre Pacaud <apacaud.externe@macif.fr>
2022-05-13 17:37:44 +0200

    refacto: modelViewDispo

--- src/Presentation/views/RendezVous/ModelView/Disponibilites/DisponibilitesModelViewBuilder.ts
+++ src/Presentation/views/RendezVous/ModelView/Disponibilites/DisponibilitesModelViewBuilder.ts
@@ -1,11 +1,27 @@
 import {DisponibilitesModelView} from "./DisponibilitesModelView";
 import Disponibilites from "../../../../../Domain/Model/Disponibilites/Disponibilites";
+import HeureDisponibleState from "../../../../../Domain/Model/Disponibilites/HeureDisponibleState";
+import HeureDisponibleModelView from "./HeureDisponibleModelView";
 
 export default class DisponibilitesModelViewBuilder {
-    static buildFromDisponibilites(disponibilites: Disponibilites) {
+    static buildHeureDispo(heureDispo: HeureDisponibleState): HeureDisponibleModelView {
+        return {
+            code: heureDispo.valeur.toString(),
+            libelle: heureDispo.libelle
+        }
+    }
+
+    static buildFromDisponibilites(disponibilites: Disponibilites): DisponibilitesModelView {
         return {
             aucuneDisponibilite: disponibilites.state.aucuneDisponibilite,
-            disponibilites: disponibilites.state.disponibilites
+            disponibilites: disponibilites.state.disponibilites.map(value => {
+                return {
+                    disponibilitesApresMidi: value.disponibilitesApresMidi.map(DisponibilitesModelViewBuilder.buildHeureDispo),
+                    disponibilitesMatin: value.disponibilitesMatin.map(DisponibilitesModelViewBuilder.buildHeureDispo),
+                    ferie: value.ferie,
+                    jour: value.jour
+                }
+            })
         }
     }
 
--- src/Presentation/views/RendezVous/ModelView/Disponibilites/HeureDisponibleModelView.ts
+++ src/Presentation/views/RendezVous/ModelView/Disponibilites/HeureDisponibleModelView.ts
@@ -1,4 +1,4 @@
 export default interface HeureDisponibleModelView {
     libelle: string;
-    valeur: number;
+    code: string;
 }
