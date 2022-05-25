7bd396818aa99df2a902d20236262f1c6619fbcf
Alexandre Pacaud <apacaud.externe@macif.fr>
2022-05-25 16:34:12 +0200

    fix: marging Form

--- src/Presentation/pages/RendezVous/HeureSwitcher/HeureSwitcher.tsx
+++ src/Presentation/pages/RendezVous/HeureSwitcher/HeureSwitcher.tsx
@@ -1,5 +1,5 @@
 import JourDisponibleModelView from "../ModelView/Disponibilites/JourDisponibleModelView";
-import {Alert, Card, Form} from "macif-components";
+import {Card, Form} from "macif-components";
 import ChoiceSwitcher from "../../../components/ChoiceSwitcher";
 import LoadWaitingIsOver from "../../../commons/LoadingEvent/LoadWaitingIsOver";
 import useLoaderObservable from "../../../hooks/useLoaderObservable";
@@ -27,37 +27,39 @@ export default function HeureSwitcher({
 
     const {isOver}: LoadWaitingIsOver = useLoaderObservable(onLoadDisponibilitesObserver);
 
-    return proposerChoixHoraire && isOver ? (<Card body bg="gris-lune">
-        <Form as={'div'}>
-            <Form.Group>
-                <Form.Label>Puis votre horaire</Form.Label>
-                <Form.Text muted>La liste n'affiche que les horaires disponibles pour ce jour et dans cette
-                    agence. Nos rendez-vous durent en moyenne 30 minutes.</Form.Text>
-            </Form.Group>
-            <ChoiceSwitcher onChoiceSelected={onChoiceSelected}
-                            choiceSelected={choiceSelected}
-                            dataSource={disponibilitesMatin}
-                            id="heureMatin"
-                            label="Matin"
-                            nbSwitchers={6}/>
-            {disponibilitesMatin.length === 0 && (
+    return proposerChoixHoraire && isOver ? (<Card as={Form.Group} bg="gris-lune">
+        <Card.Body>
+            <Form as={'div'}>
                 <Form.Group>
-                    <Form.Label>Matin</Form.Label>
-                    <Form.Text>Il n'y a plus de rendez-vous disponible pour le matin</Form.Text>
+                    <Form.Label>Puis votre horaire</Form.Label>
+                    <Form.Text muted>La liste n'affiche que les horaires disponibles pour ce jour et dans cette
+                        agence. Nos rendez-vous durent en moyenne 30 minutes.</Form.Text>
                 </Form.Group>
-            )}
-            <ChoiceSwitcher onChoiceSelected={onChoiceSelected}
-                            choiceSelected={choiceSelected}
-                            dataSource={disponibilitesApresMidi}
-                            id="heureApresMidi"
-                            label="Après Midi"
-                            nbSwitchers={6}/>
-            {disponibilitesApresMidi.length === 0 && (
-                <Form.Group>
-                    <Form.Label>Après Midi</Form.Label>
-                    <Form.Text>Il n'y a plus de rendez-vous disponible pour l'après-midi</Form.Text>
-                </Form.Group>
-            )}
-        </Form>
+                <ChoiceSwitcher onChoiceSelected={onChoiceSelected}
+                                choiceSelected={choiceSelected}
+                                dataSource={disponibilitesMatin}
+                                id="heureMatin"
+                                label="Matin"
+                                nbSwitchers={6}/>
+                {disponibilitesMatin.length === 0 && (
+                    <Form.Group>
+                        <Form.Label>Matin</Form.Label>
+                        <Form.Text>Il n'y a plus de rendez-vous disponible pour le matin</Form.Text>
+                    </Form.Group>
+                )}
+                <ChoiceSwitcher onChoiceSelected={onChoiceSelected}
+                                choiceSelected={choiceSelected}
+                                dataSource={disponibilitesApresMidi}
+                                id="heureApresMidi"
+                                label="Après Midi"
+                                nbSwitchers={6}/>
+                {disponibilitesApresMidi.length === 0 && (
+                    <Form.Group>
+                        <Form.Label>Après Midi</Form.Label>
+                        <Form.Text>Il n'y a plus de rendez-vous disponible pour l'après-midi</Form.Text>
+                    </Form.Group>
+                )}
+            </Form>
+        </Card.Body>
     </Card>) : <></>;
 }
