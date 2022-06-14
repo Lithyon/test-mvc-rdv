109a095b22d71d2da23c6ed06db2b2ca3a6a6bc0
Alexandre Pacaud <apacaud.externe@macif.fr>
2022-06-14 16:49:54 +0200

    fix: basename router

--- src/Presentation/pages/index.tsx
+++ src/Presentation/pages/index.tsx
@@ -4,7 +4,9 @@ import PagesDetails from "./PagesDetails";
 import Authentification from "./Authentification";
 
 export default function Pages() {
-    return <BrowserRouter>
+    const [serverPath] = window.location.pathname.split(/\/app/gi);
+
+    return <BrowserRouter basename={serverPath}>
         <Routes>
             <Route path={PagesDetails.RendezVous.link} element={<RendezVous/>}/>
             <Route path={PagesDetails.Auth.link} element={<Authentification/>}/>
